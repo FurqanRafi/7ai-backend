7f28425156057063acd4236f314527380502294d
Muhammad Furqan <furqanf64123@gmail.com>
2025-10-16 06:54:10 +0500

    ok done

--- controller/aiUserController.js
+++ controller/aiUserController.js
@@ -9,7 +9,7 @@ export const login = async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: "User Not Found" });
     }
-    const isMatch = bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid Password" });
     }
